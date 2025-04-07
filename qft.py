5b5b378e63e531650a017d5763b13ffdc3e00268
carlfre <carlfrederiknk@gmail.com>
2025-04-07 15:36:35 +0200

    inverse qft

--- qft.py
+++ qft.py
@@ -5,6 +5,7 @@ import numpy as np
 
 
 def qft(n_qubits: int) -> np.ndarray:
+
     H = hadamard()
 
     QFT = np.eye(2**n_qubits, dtype=complex)
@@ -24,13 +25,32 @@ def qft(n_qubits: int) -> np.ndarray:
     return QFT
 
 
+def inverse_qft(n_qubits: int) -> np.ndarray:
+    H = hadamard()
+
+    iQFT = np.eye(2**n_qubits, dtype=complex)
+
+    for i in range(n_qubits//2 -1, -1, -1):
+        iQFT = swap(i, n_qubits - i - 1, n_qubits) @ iQFT
+        
+    
+    for i in range(n_qubits - 1, -1, -1):
+        Hi = multi_qubit_gate(H, i, n_qubits)
+        iQFT = Hi @ iQFT
+        for j in range(i - 1, -1, -1):
+            theta = -2 * np.pi / (2**(i - j + 1))
+            U = CU1(theta, j, i, n_qubits)
+            iQFT = U @ iQFT
+
+    return iQFT
+
+
 if __name__ == "__main__":
     from linear_algebra import quantum_fourier_transform
 
-    np.set_printoptions(precision=2, suppress=True, linewidth=200)
 
-    n_qubits = 5
+    N = 6
+    QFT = qft(N)
+    iQFT = inverse_qft(N)
 
-    qft_linalg = quantum_fourier_transform(2**n_qubits)
-    qft_matrix = qft(n_qubits)
-    print(np.linalg.norm(qft_linalg - qft_matrix))
+    print(np.linalg.norm(QFT @ iQFT - np.eye(16)))
